8009ba1bd9489271bd63ed94f5db8c81e1a2eda8
Niyiaremu <neyoaremu@gmail.com>
2025-07-25 12:27:50 +0100

    Add files via upload

---
+++ Home.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Staff Check-In List</title>
+</head>
+<body bgcolor="E9967A">
+    <form action="Home.php" method="post">
+        <label>Name:</label> <br>
+        <input type="text" name="name" id="name"> <br><br>
+
+        <label>Date:</label> <br>
+        <input type="date" name="date" id="date"> <br><br>
+
+        <label>Time:</label> <br>
+        <input type="time" name="time" id="time"> <br><br>
+
+        <input type="submit" name="submit" id="submit">
+    </form>
+</body>
+</html>
+
+
+<?php
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,2 @@
+"# StaffCheckin" 
+"# StaffCheckingin" 
